6ecd1919f60aee9d6b5fb62e0d69c9d4e8bfab69
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2024-01-03 12:09:01 -0300

    Upload

---
+++ DemoMVC/Controllers/AlunosController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DemoMvc.Data;
+using DemoMvc.Models;
+
+namespace DemoMvc.Controllers
+{
+    public class AlunosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlunosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Alunos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Aluno.ToListAsync());
+        }
+
+        // GET: Alunos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // GET: Alunos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alunos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Email,DataCadastro,ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aluno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        // GET: Alunos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Alunos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Email,DataCadastro,ativo")] Aluno aluno)
+        {
+            if (id != aluno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aluno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlunoExists(aluno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        // GET: Alunos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // POST: Alunos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno != null)
+            {
+                _context.Aluno.Remove(aluno);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlunoExists(int id)
+        {
+            return _context.Aluno.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DemoMVC/Models/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoMvc.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool ativo { get; set; }
+
+
+
+    }
+}
