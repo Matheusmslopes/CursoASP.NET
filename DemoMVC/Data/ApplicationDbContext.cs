f4d48c00355f371b695178b1449460f0de330101
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2024-01-03 12:10:30 -0300

    Upload

---
+++ DemoMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DemoMvc.Models;
+
+namespace DemoMvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DemoMvc.Models.Aluno> Aluno { get; set; }
+    }
+}
