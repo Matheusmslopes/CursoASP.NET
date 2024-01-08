84f2e5e78a408b4ceb5caf4a9b2d33ea5363abb7
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2024-01-08 14:40:24 -0300

    Upload

---
+++ DemoWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using DemoWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("alunos")]
+        public Aluno GetAlunos()
+        {
+            return new Aluno {
+                Id = 1,
+                Nome = "Matheus",
+                Email = "Matheus@teste.com",
+                DataCadastro = DateTime.Now,
+                Ativo = true
+            };
+        }
+    }
+}
---
+++ DemoWebApi/Models/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoWebAPI.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
