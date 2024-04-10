1ab443ed8b1eff6e933dd862cadd526810807b0c
Matheus <matheusmuruci@outlook.com>
2024-04-10 19:03:21 -0300

    V1.5 Implementing a View Component

---
+++ ConhecimentosEssenciais/src/AppSemTemplate/Components/CounterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppSemTemplate.Components
+{
+    public class CounterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int modelCounter)
+        {
+            return View(modelCounter);
+        }
+    }
+}
